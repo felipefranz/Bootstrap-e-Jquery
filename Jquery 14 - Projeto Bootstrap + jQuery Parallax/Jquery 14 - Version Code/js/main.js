d8757fca4263fa7f8c1f9cc8876a8705b61f3fe0
Felipe Dutra <felipe.franz.silva@hotmail.com>
2020-06-02 15:28:08 -0300

    Comentários Animated.css e Parallax

--- Jquery 14 - Projeto Bootstrap + jQuery Parallax/Jquery 14 - Version Code/js/main.js
+++ Jquery 14 - Projeto Bootstrap + jQuery Parallax/Jquery 14 - Version Code/js/main.js
@@ -1,22 +1,20 @@
 (function($){
     
-    $('.paroller').paroller()
-    var contentWaypoint = function(){
-        $('.element-animate').waypoint( function( direction ){
+    $('.paroller').paroller() //intancia da função do evento parallax
+    var contentWaypoint = function(){  
+        $('.element-animate').waypoint( function( direction ){ //waypoint captura a position do scroll
             console.log(direction)
             console.log(this)
             console.log(this.element)
             
             const $element = $(this.element);
-            let effect = $element.data('animate-effect') || 'fadeInUp';
+            let effect = $element.data('animate-effect') || 'fadeInUp'; //effect recebe $element.data('animate-effect') ou 'fadeInUp'
                         
-            
-            
-            if(direction === 'down' && !$element.hasClass('element-animated')){
-                $element.removeClass('element-animate').addClass('element-animated ' + effect)
+            if(direction === 'down' && !$element.hasClass('element-animated')){ //se direction for down e elemento não tiver com a classe element-animated (flag de animada)
+                $element.removeClass('element-animate').addClass('element-animated ' + effect) //remove classe element-animate e adiciona efeito de animação
             }
         }, {
-            offset: '90%'
+            offset: '90%' //Ajuste para efeito ocorrer em posições mais adequadas ao scroll, Acrescenta a distancia do objeto em relação as margens do DOM
         })
     }
     
